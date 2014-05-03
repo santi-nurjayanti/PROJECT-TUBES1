f15bda4058c14e05e66507330317b560c9a60655
evisseptriana <evisseptriana@gmail.com>
2014-05-04 06:11:45 +0700

    Update parsing_diantaranews.php

--- parsing_diantaranews.php
+++ parsing_diantaranews.php
@@ -1,11 +1,20 @@
+<html>
+<head>
+<body>
+<h1>"DIANTARANEWS.COM"</h1>
 <?php
-$xml = simplexml_load_file("http://www.antara.co.id/rss/news.xml")
+$xml = simplexml_load_file("xml/news.xml")
        or die("Error: Cannot create object");
+	  
 foreach($xml->children() as $item){
   foreach($item->children() as $item => $data){
-     echo $data['title']. "<br/>";
-     echo $data->pubDate . "<br/>";
-     echo $data->description . "<br/>";
+     
+	 echo $data['title']. '<br/>';
+     echo $data->pubDate . '<br/>';
+     echo $data->description . '<br/>';
   }
 }      
 ?>
+</body>
+</head>
+</html>

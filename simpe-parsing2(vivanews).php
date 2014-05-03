267ee4d7787ff4062b0bfb76e9a05378f47cfcd3
evisseptriana <evisseptriana@gmail.com>
2014-05-04 06:09:30 +0700

    Delete simpe-parsing2(vivanews).php

--- simpe-parsing2(vivanews).php
+++
@@ -1,9 +0,0 @@
-<?php
-$xml = simplexml_load_file("http://rss.vivanews.com/get/bola");
-$items = $xml->xpath("/rss/channel/item");
-foreach($items as $item){
-   echo $item ->pubDate.'<br>'; 
-   echo  $item->title.'';
-   echo $item->description.'';
-}
-?>
